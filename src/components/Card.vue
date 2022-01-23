b0c45a3021861655aa20310c5325c253540fde1c
priyank-hub <54325230+priyank-hub@users.noreply.github.com>
2022-01-23 18:22:15 -0500

    added repos section

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -63,210 +63,221 @@
 
       <div class="row my-3">
         <div class="col-12">
-            <v-card
-              elevation="4"
-              class="section p-4 p-md-5"
-            >
-              <div class="row">           
-                <div class="col-12 col-md-3 col-lg-2">
-                  <b-img :src="user.picture" class="" style="width: 100%" rounded="circle" alt="Circle image"></b-img>
-                </div>
-                
-                <div class="col-12 col-md-9 col-lg-10 text-md-left text-lg-left">              
-                  <div class="row justify-content-between align-items-center">
-                    <div class="col-12 col-md-6 col-lg-6 pb-0">
-                      <span style="font-size: 24px; font-weight: 900; letter-spacing: 1px" v-if="user.name">
-                        {{ user.name }}
-                      </span>
-                      <span v-else class="text-muted" style="font-size: 24px; font-weight: 900; letter-spacing: 1px">
-                        Name Not Available
-                      </span>
-                    </div>
-                    <div class="col-12 col-md-6 col-lg-6 py-0 text-lg-right">
-                      <span style="font-size: 14px; font-weight: 700; letter-spacing: 1px">
-                        Joined 
-                        <span v-if="user.joined_date">
-                          {{ joined(user.joined_date) }}
-                        </span>
-                        <span v-else class="text-muted">
-                          Not Available
-                        </span>
-                      </span>
-                    </div>
+          <v-card
+            elevation="4"
+            class="section p-4 p-md-5"
+          >
+            <div class="row">           
+              <div class="col-12 col-md-3 col-lg-2">
+                <b-img :src="user.picture" class="" style="width: 100%" rounded="circle" alt="Circle image"></b-img>
+              </div>
+              
+              <div class="col-12 col-md-9 col-lg-10 text-md-left text-lg-left">              
+                <div class="row justify-content-between align-items-center">
+                  <div class="col-12 col-md-6 col-lg-6 pb-0">
+                    <span style="font-size: 24px; font-weight: 900; letter-spacing: 1px" v-if="user.name">
+                      {{ user.name }}
+                    </span>
+                    <span v-else class="text-muted" style="font-size: 24px; font-weight: 900; letter-spacing: 1px">
+                      Name Not Available
+                    </span>
                   </div>
+                  <div class="col-12 col-md-6 col-lg-6 py-0 text-lg-right">
+                    <span style="font-size: 14px; font-weight: 700; letter-spacing: 1px">
+                      Joined 
+                      <span v-if="user.joined_date">
+                        {{ joined(user.joined_date) }}
+                      </span>
+                      <span v-else class="text-muted">
+                        Not Available
+                      </span>
+                    </span>
+                  </div>
+                </div>
 
-                  <div class="row">
-                    <div class="col-12 py-0">
-                      <span style="font-size: 15px" class="text-primary">
-                        @{{ user.username }}
-                      </span>
-                    </div>
-                  </div>  
-                
-                  <div class="row my-3 d-none d-md-none d-lg-block">
-                    <div class="col-12 col-md-10">
-                      <span class="text-muted" v-if="!user.bio">
-                        This profile has no bio.
-                      </span>
-                      <span v-else>
-                        {{ user.bio }}
-                      </span>
-                    </div>
+                <div class="row">
+                  <div class="col-12 py-0">
+                    <span style="font-size: 15px" class="text-primary">
+                      @{{ user.username }}
+                    </span>
+                  </div>
+                </div>  
+              
+                <div class="row my-3 d-none d-md-none d-lg-block">
+                  <div class="col-12 col-md-10">
+                    <span class="text-muted" v-if="!user.bio">
+                      This profile has no bio.
+                    </span>
+                    <span v-else>
+                      {{ user.bio }}
+                    </span>
                   </div>
                 </div>
               </div>
+            </div>
 
-              <div class="row my-3 d-block d-md-block d-lg-none">
-                <div class="col-12 col-md-10">
-                  <span class="text-muted" v-if="!user.bio">
-                    This profile has no bio.
-                  </span>
-                  <span v-else class="text-muted">
-                    {{ user.bio }}
-                  </span>
-                </div>
+            <div class="row my-3 d-block d-md-block d-lg-none">
+              <div class="col-12 col-md-10">
+                <span class="text-muted" v-if="!user.bio">
+                  This profile has no bio.
+                </span>
+                <span v-else class="text-muted">
+                  {{ user.bio }}
+                </span>
               </div>
+            </div>
 
-              <div class="row my-2">
-                <div class="col-4 col-md-3 col-lg-2 d-none d-md-none d-lg-block py-0"></div>
-                <div class="col-12 col-md-12 col-lg-10 py-0 inner-card">
-                  
-                  <v-card
-                    elevation="4"
-                    class="section"
-                  >
-                    <div class="container my-2">
-                      <div class="row">
-                        <div class="col-12 col-md-4 text-center text-md-center text-lg-left">
-                          <div>
-                            <span style="font-size: 13px">
-                              Repos
-                            </span>
-                          </div>
+            <div class="row my-2">
+              <div class="col-4 col-md-3 col-lg-2 d-none d-md-none d-lg-block py-0"></div>
+              <div class="col-12 col-md-12 col-lg-10 py-0 inner-card">
+                
+                <v-card
+                  elevation="4"
+                  class="section"
+                >
+                  <div class="container my-2">
+                    <div class="row">
+                      <div class="col-12 col-md-4 text-center text-md-center text-lg-left">
+                        <a href="#repos" :class="this.$vuetify.theme.dark ? 'text-white' : 'text-dark'">
+                          <span style="font-size: 13px">
+                            Repos
+                          </span>
                           <div>
                             <span style="font-size: 20px; font-weight: 900">
                               {{ user.repos }}
                             </span>
                           </div>
-                        </div>
+                        </a>
+                      </div>
 
-                        <div class="col-12 col-md-4 text-center text-md-center text-lg-center">
-                          <div>
-                            <span style="font-size: 13px">
-                              Followers
-                            </span>
-                          </div>
-                          <div>
-                            <span style="font-size: 20px; font-weight: 900">
-                              {{ user.followers }}
-                            </span>
-                          </div>
+                      <div class="col-12 col-md-4 text-center text-md-center text-lg-center">
+                        <div>
+                          <span style="font-size: 13px">
+                            Followers
+                          </span>
                         </div>
+                        <div>
+                          <span style="font-size: 20px; font-weight: 900">
+                            {{ user.followers }}
+                          </span>
+                        </div>
+                      </div>
 
-                        <div class="col-12 col-md-4 text-center text-md-center text-lg-right">
-                          <div>
-                            <span style="font-size: 13px">
-                              Following
-                            </span>
-                          </div>
-                          <div>
-                            <span style="font-size: 20px; font-weight: 900">
-                              {{ user.following }}
-                            </span>
-                          </div>
+                      <div class="col-12 col-md-4 text-center text-md-center text-lg-right">
+                        <div>
+                          <span style="font-size: 13px">
+                            Following
+                          </span>
+                        </div>
+                        <div>
+                          <span style="font-size: 20px; font-weight: 900">
+                            {{ user.following }}
+                          </span>
                         </div>
                       </div>
                     </div>
-                  </v-card>
+                  </div>
+                </v-card>
 
-                </div>
               </div>
+            </div>
 
-              <div class="row my-0">
-                <div class="col-4 col-md-3 col-lg-2 d-none d-md-none d-lg-block"></div>
-                <div class="col-12 col-md-12 col-lg-10">
-                  <div class="row">
-                    <div class="col-12 col-md-6 col-lg-6 my-0 pb-0">
-                      <span v-if="user.location">
-                        <span>
-                          <i class="fas fa-map-marker-alt"></i>
-                        </span>
-                        <span class="mx-3">
-                          {{ user.location }}
-                        </span>
+            <!-- icons -->
+            <div class="row my-0">
+              <div class="col-4 col-md-3 col-lg-2 d-none d-md-none d-lg-block"></div>
+              <div class="col-12 col-md-12 col-lg-10">
+                <div class="row">
+                  <div class="col-12 col-md-6 col-lg-6 my-0 pb-0">
+                    <span v-if="user.location">
+                      <span>
+                        <i class="fas fa-map-marker-alt"></i>
                       </span>
-                      <span v-else class="text-muted">
-                        <span>
-                          <i class="fas fa-map-marker-alt"></i>
-                        </span>
-                        <span class="mx-3">
-                          Not Available
-                        </span>
+                      <span class="mx-3">
+                        {{ user.location }}
                       </span>
-                    </div>
+                    </span>
+                    <span v-else class="text-muted">
+                      <span>
+                        <i class="fas fa-map-marker-alt"></i>
+                      </span>
+                      <span class="mx-3">
+                        Not Available
+                      </span>
+                    </span>
+                  </div>
 
-                    <div class="col-12 col-md-6 col-lg-6 my-0 pb-0">
-                      <span class="" v-if="user.twitter">
-                        <span>
-                          <i class="fab fa-twitter"></i>
-                        </span>
-                        <span class="mx-3">
-                          {{ user.twitter }}
-                        </span>
+                  <div class="col-12 col-md-6 col-lg-6 my-0 pb-0">
+                    <span class="" v-if="user.twitter">
+                      <span>
+                        <i class="fab fa-twitter"></i>
                       </span>
-                      <span v-else class="text-muted">
-                        <span>
-                          <i class="fab fa-twitter"></i>
-                        </span>
-                        <span class="mx-3">
-                          Not Available
-                        </span>
+                      <span class="mx-3">
+                        {{ user.twitter }}
                       </span>
-                    </div>
+                    </span>
+                    <span v-else class="text-muted">
+                      <span>
+                        <i class="fab fa-twitter"></i>
+                      </span>
+                      <span class="mx-3">
+                        Not Available
+                      </span>
+                    </span>
+                  </div>
 
-                    <div class="col-12 col-md-6 col-lg-6 my-0 pb-0">
-                      <span class="" v-if="user.link">
-                        <span>
-                          <i class="fas fa-link"></i>
-                        </span>
-                        <span class="mx-3">
-                          <a :href="user.link" target="_blank">
-                            @{{ user.username }}
-                          </a>
-                        </span>
+                  <div class="col-12 col-md-6 col-lg-6 my-0 pb-0">
+                    <span class="" v-if="user.link">
+                      <span>
+                        <i class="fas fa-link"></i>
                       </span>
-                      <span v-else class="text-muted">
-                        <span>
-                          <i class="fas fa-link"></i>
-                        </span>
-                        <span class="mx-3">
-                          Not Available
-                        </span>
+                      <span class="mx-3">
+                        <a :href="user.link" target="_blank">
+                          @{{ user.username }}
+                        </a>
                       </span>
-                    </div>
+                    </span>
+                    <span v-else class="text-muted">
+                      <span>
+                        <i class="fas fa-link"></i>
+                      </span>
+                      <span class="mx-3">
+                        Not Available
+                      </span>
+                    </span>
+                  </div>
 
-                    <div class="col-12 col-md-6 col-lg-6 my-0">
-                      <span class="" v-if="user.email">
-                        <span>
-                          <i class="fas fa-envelope"></i>
-                        </span>
-                        <span class="mx-3">
-                          {{ user.email }}
-                        </span>
+                  <div class="col-12 col-md-6 col-lg-6 my-0">
+                    <span class="" v-if="user.email">
+                      <span>
+                        <i class="fas fa-envelope"></i>
                       </span>
-                      <span v-else class="text-muted">
-                        <span>
-                          <i class="fas fa-envelope"></i>
-                        </span>
-                        <span class="mx-3">
-                          Not Available
-                        </span>
+                      <span class="mx-3">
+                        {{ user.email }}
                       </span>
-                    </div>
+                    </span>
+                    <span v-else class="text-muted">
+                      <span>
+                        <i class="fas fa-envelope"></i>
+                      </span>
+                      <span class="mx-3">
+                        Not Available
+                      </span>
+                    </span>
                   </div>
                 </div>
               </div>
-            </v-card>
+            </div>
+          </v-card>
+        </div>
+      </div>
+
+      <div class="row my-5" v-if="repos.length > 0">
+        <div class="col-12">
+          <v-card elevation="4" class="section p-4 p-md-5">
+            <section id="repos">
+              <Repos :repos="repos" :user="user"/>
+            </section>
+          </v-card>
         </div>
       </div>
     </div>
@@ -277,13 +288,16 @@
 
 import axios from 'axios'
 import moment from 'moment'
+import Repos from './Repos'
 
 export default {
   name: 'Card',
   props: {
   
   },
-
+  components: {
+    Repos,
+  },
   data() {
     return {
       search: '',
@@ -305,6 +319,8 @@ export default {
         institution: null,
         picture: '/assets/user.png',
       },
+
+      repos: [],
       error: false,
     }
   },
@@ -315,10 +331,10 @@ export default {
     },
     async searchUser() {
       this.error = false;
-      console.log('searching...')
+      this.repos = [];
       try {
         let res =  await axios.get(`https://api.github.com/users/${this.search}`)
-        console.log('res', res);
+        // console.log('res', res);
         this.user.name = res.data.name;
         this.user.email = res.data.email;
         this.user.joined_date = res.data.created_at;
@@ -335,32 +351,14 @@ export default {
         this.user.link = res.data.html_url;
         this.user.institution = res.data.organizations_url;
         this.user.picture = res.data.avatar_url;
-
-        // .then(res => {
-          // this.user.name = res.data.name;
-          // this.user.email = res.data.email;
-          // this.user.joined_date = res.data.created_at;
-          // this.user.username = res.data.login;
-          // this.user.bio = res.data.bio;
-          // this.user.repos = res.data.public_repos;
-          // this.user.repos_url = res.data.repos_url;
-          // this.user.followers = res.data.followers;
-          // this.user.following_url = res.data.following_url;
-          // this.user.following = res.data.following;
-          // this.user.followers_url = res.data.followers_url;
-          // this.user.location = res.data.location;
-          // this.user.twitter = res.data.twitter_username;
-          // this.user.link = res.data.html_url;
-          // this.user.institution = res.data.organizations_url;
-          // this.user.picture = res.data.avatar_url;
-        // }
       } catch (error) {
         console.log('error', error);
         this.error = true;
       }
 
-      // const repos = await axios.get('https://api.github.com/users/priyank-hub/repos');
-      // console.log(repos);
+      const repo = await axios.get('https://api.github.com/users/' + this.user.username + '/repos');
+      console.log(repo.data);
+      this.repos = repo.data;
     },
     joined(date) {
       console.log();
---
+++ src/components/Repos.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div class="">
+            <span class="" style="font-size: 3vh; font-weight: 900; letter-spacing: 1px">
+                <i class="fab fa-git-alt"></i>
+                <span class="">
+                    Repositories
+                </span>
+            </span>
+        </div>
+        <div class="inner-card my-3">
+            <v-card
+                elevation="4"
+                class="section my-4 p-3"
+                v-for="(repo, index) in repos" :key="index"
+            >
+                {{ repo.name }}
+            </v-card>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Repos',
+    components: {
+
+    },
+    props: {
+        repos: Array,
+        user: Object,
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+
+}
+</script>
